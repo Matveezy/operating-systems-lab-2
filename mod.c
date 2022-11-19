59e8f09c1fee4baf514fcccf83ae3c08babd583d
Matvey Chukhno <mr.chucno@yandex.ru>
2022-11-19 20:23:28 +0300

    Code style fixing

--- mod.c
+++ mod.c
@@ -10,6 +10,7 @@
 
 #include "my_pci.c"
 #include "./ioctl/ioctl_cmd.h"
+
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Matvey Chukhno");
 MODULE_DESCRIPTION("My kernel module.");
@@ -23,22 +24,23 @@ static struct socket *sock = NULL;
 static my_socket_struct my_sock;
 int err = 1;
 
-static long int my_ioctl(struct file* file , unsigned cmd, unsigned long arg){
+static long int my_ioctl(struct file *file, unsigned cmd, unsigned long arg) {
     static struct pci_dev *device = NULL;
     pci_dev_params user_params;
     int32_t sockfd;
-    switch (cmd){
+    switch (cmd) {
         case PCI_DEV_W_PARAMS:
-            if (copy_from_user(&user_params , (pci_dev_params *) arg , sizeof(pci_dev_params))){
+            if (copy_from_user(&user_params, (pci_dev_params *) arg, sizeof(pci_dev_params))) {
                 printk("ioctl - PCI_DEV_W_PARAMS error!\n");
             }
-            device = pci_get_device(user_params.vendor_id , user_params.device_id , NULL);
-            if (NULL == device){
+            device = pci_get_device(user_params.vendor_id, user_params.device_id, NULL);
+            if (NULL == device) {
                 my_device.ioctl_state = ERROR;
-                printk(KERN_INFO "Pci device is not found!\n");
-            } 
-            else {
-                printk(KERN_INFO "Pci device found!\n");
+                printk(KERN_INFO
+                "Pci device is not found!\n");
+            } else {
+                printk(KERN_INFO
+                "Pci device found!\n");
                 my_device.ioctl_state = EXCELLENT;
                 my_device.devfn = device->devfn;
                 my_device.vendor = device->vendor;
@@ -46,84 +48,85 @@ static long int my_ioctl(struct file* file , unsigned cmd, unsigned long arg){
                 my_device.class = device->class;
                 my_device.driver_name = device->driver->name;
                 print_info_pci_dev_k(&my_device);
-                }
+            }
             break;
         case SOCKET_FD_W_VALUE:
-            if (copy_from_user(&sockfd , (int32_t *) arg , sizeof (sockfd))){
+            if (copy_from_user(&sockfd, (int32_t *) arg, sizeof(sockfd))) {
                 printk("ioctl - Error copying data from user!\n");
-            }
-            else {
-                printk("ioctl - socket file descriptor %d\n" , sockfd);
-                sock = sockfd_lookup(sockfd , &err);
-                    if (NULL == sock){
-                        my_sock.ioctl_state = ERROR;
-                        printk(KERN_INFO "Socket is not found!\n");
-                    }
-                    else {
-                        printk("Socket is found!\n");
-                        my_sock.ioctl_state = EXCELLENT;
-                        my_sock.type = sock->type;
-                        my_sock.flags = sock->flags;
-                        printk("Socket type is : %hx\n" , sock->type);
-                        printk("Socket flags is : %lu\n" , sock->flags);
-                    }
+            } else {
+                printk("ioctl - socket file descriptor %d\n", sockfd);
+                sock = sockfd_lookup(sockfd, &err);
+                if (NULL == sock) {
+                    my_sock.ioctl_state = ERROR;
+                    printk(KERN_INFO
+                    "Socket is not found!\n");
+                } else {
+                    printk("Socket is found!\n");
+                    my_sock.ioctl_state = EXCELLENT;
+                    my_sock.type = sock->type;
+                    my_sock.flags = sock->flags;
+                    printk("Socket type is : %hx\n", sock->type);
+                    printk("Socket flags is : %lu\n", sock->flags);
                 }
+            }
             break;
         case SOCKET_R_VALUE:
-            if (copy_to_user((my_socket_struct *) arg , &my_sock , sizeof (my_socket_struct))){
+            if (copy_to_user((my_socket_struct *) arg, &my_sock, sizeof(my_socket_struct))) {
                 printk("ioctl - Error copying data to user!\n");
-            }
-            else {
+            } else {
                 printk("ioctl - The socket_r_value command called!\n");
             }
             break;
         case PCI_DEV_R_VALUE:
-            if (copy_to_user((my_pci_dev_struct *) arg , &my_device , sizeof (my_pci_dev_struct))){
-                    printk("ioctl - Error SOCKET_R_VALUE!\n");
+            if (copy_to_user((my_pci_dev_struct *) arg, &my_device, sizeof(my_pci_dev_struct))) {
+                printk("ioctl - Error SOCKET_R_VALUE!\n");
             }
             break;
-    
+
     }
     return 0;
 }
 
-static int driver_open(struct inode *device_file , struct file *inctance){
+static int driver_open(struct inode *device_file, struct file *inctance) {
     printk("dev_num - open was called\n");
     return 0;
 }
 
-static int driver_close(struct inode *device_file , struct file *inctance){
+static int driver_close(struct inode *device_file, struct file *inctance) {
     printk("dev_num - close was called\n");
     return 0;
 }
 
 static struct file_operations fops = {
-    .owner = THIS_MODULE,
-    .open = driver_open,
-    .release = driver_close,
-    .unlocked_ioctl = my_ioctl
+        .owner = THIS_MODULE,
+        .open = driver_open,
+        .release = driver_close,
+        .unlocked_ioctl = my_ioctl
 };
 
-static int __init mod_init(void) {
+static int __init
+
+mod_init(void) {
     /* register device number */
     int retval;
-    retval = register_chrdev(MYMAJOR , "matthew_driver" , &fops);
+    retval = register_chrdev(MYMAJOR, "matthew_driver", &fops);
     if (0 == retval) {
-        printk("matthew_driver device number Major:%d , Minor:%d\n" , MYMAJOR , 0);
-    }
-    else if (retval >0 ){
-        printk("matthew_driver device number Major:%d , Minor:%d\n" , retval>>20 , retval&0xffff);
-    }
-    else {
+        printk("matthew_driver device number Major:%d , Minor:%d\n", MYMAJOR, 0);
+    } else if (retval > 0) {
+        printk("matthew_driver device number Major:%d , Minor:%d\n", retval >> 20, retval & 0xffff);
+    } else {
         printk("Couldn't register device number!\n");
         return -1;
     }
- return 0;
+    return 0;
 }
 
-static void __exit mod_exit(void) {
-    unregister_chrdev(MYMAJOR , "matthew_driver");
-    printk(KERN_INFO "Goodbye, World!\n");
+static void __exit
+
+mod_exit(void) {
+    unregister_chrdev(MYMAJOR, "matthew_driver");
+    printk(KERN_INFO
+    "Goodbye, World!\n");
 }
 
 module_init(mod_init);
--- user_side.c
+++ user_side.c
@@ -8,60 +8,59 @@
 
 #include "./ioctl/ioctl_cmd.h"
 
-void print_info_pci_dev_u(my_pci_dev_struct*);
-void print_info_socket_u(my_socket_struct*);
+void print_info_pci_dev_u(my_pci_dev_struct *);
 
-int main (int argc , char **argv) {
-	if (argc < 2){
-		return -1;
-	}
+void print_info_socket_u(my_socket_struct *);
 
-	unsigned int vendor_id = (unsigned int) strtol(argv[1] , NULL, 16);
-	unsigned int device_id = (unsigned int) strtol(argv[2] , NULL, 16);
-	pci_dev_params pci_dev_prms = {.vendor_id = vendor_id , .device_id = device_id};
-	my_pci_dev_struct my_dev;
-	my_socket_struct my_sock;
-	int dev = open("/dev/matthew_device" , O_WRONLY);
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        return -1;
+    }
 
-	if (-1 == dev) {
-		printf("Opening device file wasn't possible!\n");
-		return -1;
-	}
+    unsigned int vendor_id = (unsigned int) strtol(argv[1], NULL, 16);
+    unsigned int device_id = (unsigned int) strtol(argv[2], NULL, 16);
+    pci_dev_params pci_dev_prms = {.vendor_id = vendor_id, .device_id = device_id};
+    my_pci_dev_struct my_dev;
+    my_socket_struct my_sock;
+    int dev = open("/dev/matthew_device", O_WRONLY);
 
-	ioctl(dev ,PCI_DEV_W_PARAMS , &pci_dev_prms);
-	ioctl(dev , PCI_DEV_R_VALUE , &my_dev);
-	if (my_dev.ioctl_state = EXCELLENT) {
-		print_info_pci_dev_u(&my_dev);
-	}
-	else {
-		printf("There is no devices with such params!\n");
-	}
+    if (-1 == dev) {
+        printf("Opening device file wasn't possible!\n");
+        return -1;
+    }
 
-	int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-    printf("Socket fd is : %d\n" , sockfd);
-	ioctl(dev , SOCKET_FD_W_VALUE , &sockfd);
+    ioctl(dev, PCI_DEV_W_PARAMS, &pci_dev_prms);
+    ioctl(dev, PCI_DEV_R_VALUE, &my_dev);
+    if (my_dev.ioctl_state = EXCELLENT) {
+        print_info_pci_dev_u(&my_dev);
+    } else {
+        printf("There is no devices with such params!\n");
+    }
 
-	ioctl(dev , SOCKET_R_VALUE , &my_sock);
-	if (my_sock.ioctl_state == EXCELLENT) {
-		print_info_socket_u(&my_sock);
-	}
-	else {
-		printf("There is no socket with the fd = %d\n" , sockfd);
-	}
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    printf("Socket fd is : %d\n", sockfd);
+    ioctl(dev, SOCKET_FD_W_VALUE, &sockfd);
 
-	printf("Opening device file was successfull!\n");
-	close(dev);
-	return 0;
+    ioctl(dev, SOCKET_R_VALUE, &my_sock);
+    if (my_sock.ioctl_state == EXCELLENT) {
+        print_info_socket_u(&my_sock);
+    } else {
+        printf("There is no socket with the fd = %d\n", sockfd);
+    }
+
+    printf("Opening device file was successfull!\n");
+    close(dev);
+    return 0;
 }
 
-void print_info_pci_dev_u(my_pci_dev_struct *my_device){
-        printf("Device devfn field is : %u\n" , my_device->devfn);
-        printf("Device vendor field is : %hu\n" , my_device->vendor);
-        printf("Device deivce field is : %hu\n" , my_device->device);
-        printf("Device class field is : %u\n" , my_device->class);
+void print_info_pci_dev_u(my_pci_dev_struct *my_device) {
+    printf("Device devfn field is : %u\n", my_device->devfn);
+    printf("Device vendor field is : %hu\n", my_device->vendor);
+    printf("Device deivce field is : %hu\n", my_device->device);
+    printf("Device class field is : %u\n", my_device->class);
 }
 
-void print_info_socket_u(my_socket_struct *my_socket){
-		printf("Socket type is: %hx\n" , my_socket->type);
-		printf("Socket flags is: %lu\n" , my_socket->flags);
+void print_info_socket_u(my_socket_struct *my_socket) {
+    printf("Socket type is: %hx\n", my_socket->type);
+    printf("Socket flags is: %lu\n", my_socket->flags);
 }
